944fc30175e1e84903f0dbf71ef9b2c48b536dcd
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2023-08-19 11:48:18 +0300

    migrate to typescrip-tools.nvim

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,74 +1,93 @@
 local plugins = {
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{
-				"hrsh7th/cmp-nvim-lsp",
-				-- enabled = function()
-				-- 	return require("lazy.core.config").plugins["nvim-cmp"] ~= nil
-				-- end,
-			},
-			{ "folke/neodev.nvim", config = true },
-			-- {
-			-- 	"jose-elias-alvarez/typescript.nvim",
-			-- 	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" },
-			-- },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      {
+        "hrsh7th/cmp-nvim-lsp",
+        -- enabled = function()
+        -- 	return require("lazy.core.config").plugins["nvim-cmp"] ~= nil
+        -- end,
+      },
+      { "folke/neodev.nvim", config = true },
+      -- {
+      -- 	"jose-elias-alvarez/typescript.nvim",
+      -- 	ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" },
+      -- },
       {
         "pmizio/typescript-tools.nvim",
-				ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" },
-        config = true
+        ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        config = function()
+          require("typescript-tools").setup({})
+          local actions = {
+            ["Organize Imports"] = "TSToolsOrganizeImports",
+            ["Sort Imports"] = "TSToolsSortImports",
+            ["Remove Unused Imports"] = "TSToolsRemoveUnused",
+            ["Go to Source Definition"] = "TSToolsGoToSourceDefinition",
+            ["Add Missing Imports"] = "TSToolsAddMissingImports",
+            ["Fix All"] = "TSToolsFixAll",
+          }
+          local function code_action()
+            vim.ui.select(vim.tbl_keys(actions), {
+              prompt = "Select Action",
+            }, function(selection)
+              vim.cmd(actions[selection])
+            end)
+          end
+
+          vim.keymap.set("n", "<leader>ca", code_action, { silent = true, noremap = true })
+        end,
       },
-			"williamboman/mason.nvim",
-		},
-		config = function()
-			require("lsp.lang_servers")
-		end,
-	},
-	-- {
-	-- 	"Exafunction/codeium.vim",
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		vim.g.codeium_disable_bindings = 1
-	-- 		vim.keymap.set("i", "<C-g>", function()
-	-- 			return vim.fn["codeium#Accept"]()
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-;>", function()
-	-- 			return vim.fn["codeium#CycleCompletions"](1)
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-,>", function()
-	-- 			return vim.fn["codeium#CycleCompletions"](-1)
-	-- 		end, { expr = true, silent = true })
-	-- 		vim.keymap.set("i", "<c-x>", function()
-	-- 			return vim.fn["codeium#Clear"]()
-	-- 		end, { expr = true, silent = true })
-	-- 	end,
-	-- },
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		cmd = "Copilot",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					keymap = {
-						accept = "<C-g>",
-					},
-				},
-			})
-		end,
-	},
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("lsp.lang_servers")
+    end,
+  },
+  -- {
+  -- 	"Exafunction/codeium.vim",
+  -- 	event = "InsertEnter",
+  -- 	config = function()
+  -- 		vim.g.codeium_disable_bindings = 1
+  -- 		vim.keymap.set("i", "<C-g>", function()
+  -- 			return vim.fn["codeium#Accept"]()
+  -- 		end, { expr = true, silent = true })
+  -- 		vim.keymap.set("i", "<c-;>", function()
+  -- 			return vim.fn["codeium#CycleCompletions"](1)
+  -- 		end, { expr = true, silent = true })
+  -- 		vim.keymap.set("i", "<c-,>", function()
+  -- 			return vim.fn["codeium#CycleCompletions"](-1)
+  -- 		end, { expr = true, silent = true })
+  -- 		vim.keymap.set("i", "<c-x>", function()
+  -- 			return vim.fn["codeium#Clear"]()
+  -- 		end, { expr = true, silent = true })
+  -- 	end,
+  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = "Copilot",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-g>",
+          },
+        },
+      })
+    end,
+  },
   {
     "folke/trouble.nvim",
-    cmd = {"TroubleToggle", "Trouble"},
+    cmd = { "TroubleToggle", "Trouble" },
     keys = {
-      {"<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>"},
-      {"<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>"},
-      {"<leader>tt", "<cmd>TroubleToggle<cr>"},
-    }
-  }
+      { "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>" },
+      { "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+      { "<leader>tt", "<cmd>TroubleToggle<cr>" },
+    },
+  },
 }
 
 local luasnip = require("plugins.lsp.luasnip")
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,121 +1,121 @@
 local config = {
-	ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "python", "lua", "vim", "vimdoc" },
-	auto_install = true,
-	highlight = {
-		enable = true,
-		disable = { "" },
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		-- additional_vim_regex_highlighting = { "html" },
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-		disable = { "python", "svelte" },
-	},
-	playground = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-    enable_close_on_slash = false
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn",
-			node_incremental = "gni",
-			scope_incremental = "gnc",
-			node_decremental = "gnm",
-		},
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>sn"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>sp"] = "@parameter.inner",
-			},
-		},
-		lsp_interop = {
-			enable = true,
-			border = "none",
-			peek_definition_code = {
-				["<leader>pf"] = "@function.outer",
-				["<leader>pc"] = "@class.outer",
-			},
-		},
-	},
-	move = {
-		enable = true,
-		set_jumps = true, -- whether to set jumps in the jumplist
-		goto_next_start = {
-			["]m"] = "@function.outer",
-			["]]"] = "@class.outer",
-		},
-		goto_next_end = {
-			["]M"] = "@function.outer",
-			["]["] = "@class.outer",
-		},
-		goto_previous_start = {
-			["[m"] = "@function.outer",
-			["[["] = "@class.outer",
-		},
-		goto_previous_end = {
-			["[M"] = "@function.outer",
-			["[]"] = "@class.outer",
-		},
-		goto_next = {
-			["]o"] = "@loop.*",
-			["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-			["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-			["]d"] = "@conditional.outer",
-		},
-		goto_previous = {
-			["[o"] = "@loop.*",
-			["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-			["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-			["[d"] = "@conditional.outer",
-		},
-	},
+  ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "python", "lua", "vim", "vimdoc" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    disable = { "" },
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    -- additional_vim_regex_highlighting = { "html" },
+    additional_vim_regex_highlighting = { "python", "svelte" },
+  },
+  indent = {
+    enable = true,
+    disable = { "python", "svelte" },
+  },
+  playground = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+    enable_close_on_slash = false,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "gni",
+      scope_incremental = "gnc",
+      node_decremental = "gnm",
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>sn"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>sp"] = "@parameter.inner",
+      },
+    },
+    lsp_interop = {
+      enable = true,
+      border = "none",
+      peek_definition_code = {
+        ["<leader>pf"] = "@function.outer",
+        ["<leader>pc"] = "@class.outer",
+      },
+    },
+  },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      ["]m"] = "@function.outer",
+      ["]]"] = "@class.outer",
+    },
+    goto_next_end = {
+      ["]M"] = "@function.outer",
+      ["]["] = "@class.outer",
+    },
+    goto_previous_start = {
+      ["[m"] = "@function.outer",
+      ["[["] = "@class.outer",
+    },
+    goto_previous_end = {
+      ["[M"] = "@function.outer",
+      ["[]"] = "@class.outer",
+    },
+    goto_next = {
+      ["]o"] = "@loop.*",
+      ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+      ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      ["]d"] = "@conditional.outer",
+    },
+    goto_previous = {
+      ["[o"] = "@loop.*",
+      ["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+      ["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      ["[d"] = "@conditional.outer",
+    },
+  },
   matchup = {
     enable = true,
-  }
+  },
 }
 
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup(config)
-		end,
-		dependencies = {
-			{ "JoosepAlviste/nvim-ts-context-commentstring", priority = 100 },
-			{ "nvim-treesitter/nvim-treesitter-textobjects" },
-			{
-				"nvim-treesitter/nvim-treesitter-context",
-				config = true,
-			},
-		},
-	},
-	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup(config)
+    end,
+    dependencies = {
+      { "JoosepAlviste/nvim-ts-context-commentstring", priority = 100 },
+      { "nvim-treesitter/nvim-treesitter-textobjects" },
+      {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = true,
+      },
+    },
+  },
+  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 }
