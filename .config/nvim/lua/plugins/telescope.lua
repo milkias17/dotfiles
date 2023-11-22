195af2743788c94c4ba24f27687fe3ce2eccc913
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2023-11-22 08:39:21 +0300

    add telescope mapping to add to harpoon

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,11 +5,14 @@ local config = {
 			i = {
 				["<C-h>"] = "which_key",
 				["<esc>"] = "close",
-        ["<C-t>"] = function(prompt_bufnr)
-          local entry = require("telescope.actions.state").get_selected_entry()
-          require("harpoon.mark").add_file(entry[1])
-          require("telescope.actions").close(prompt_bufnr)
-        end,
+				["<C-t>"] = function(prompt_bufnr)
+					local entry = require("telescope.actions.state").get_selected_entry()
+					require("harpoon.mark").add_file(entry[1])
+					require("telescope.actions").select_default(prompt_bufnr)
+					vim.schedule_wrap(function()
+						require("telescope.actions").close(prompt_bufnr)
+					end)
+				end,
 			},
 		},
 	},
@@ -55,7 +58,6 @@ end
 
 local opts = { noremap = true, silent = true }
 
-
 return {
 	{
 		"nvim-telescope/telescope.nvim",
@@ -68,12 +70,36 @@ return {
 				"nvim-telescope/telescope-media-files.nvim",
 			},
 		},
-    cmd = "Telescope",
+		cmd = "Telescope",
 		keys = {
-			{ "<space>f", function() require("telescope.builtin").find_files() end, opts },
-			{ "<space>gf", function() require("telescope.builtin").git_files() end, opts },
-			{ "<space>l", function() require("telescope.builtin").live_grep() end, opts },
-			{ "<space>b", function() require("telescope.builtin").buffers() end, opts },
+			{
+				"<space>f",
+				function()
+					require("telescope.builtin").find_files()
+				end,
+				opts,
+			},
+			{
+				"<space>gf",
+				function()
+					require("telescope.builtin").git_files()
+				end,
+				opts,
+			},
+			{
+				"<space>l",
+				function()
+					require("telescope.builtin").live_grep()
+				end,
+				opts,
+			},
+			{
+				"<space>b",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+				opts,
+			},
 			{
 				"<space>tt",
 				function()
@@ -81,20 +107,50 @@ return {
 				end,
 				opts,
 			},
-			{ "<space>tc", function() require("telescope.builtin").commands() end, opts },
-			{ "<space>th", function() require("telescope.builtin").help_tags() end, opts },
-			{ "<space>ts", function() require("telescope.builtin").grep_string() end, opts },
-			{ "<space>ds", function() require("telescope.builtin").lsp_document_symbols() end, opts },
-			{ "<space>tk", function() require("telescope.builtin").keymaps() end, opts },
+			{
+				"<space>tc",
+				function()
+					require("telescope.builtin").commands()
+				end,
+				opts,
+			},
+			{
+				"<space>th",
+				function()
+					require("telescope.builtin").help_tags()
+				end,
+				opts,
+			},
+			{
+				"<space>ts",
+				function()
+					require("telescope.builtin").grep_string()
+				end,
+				opts,
+			},
+			{
+				"<space>ds",
+				function()
+					require("telescope.builtin").lsp_document_symbols()
+				end,
+				opts,
+			},
+			{
+				"<space>tk",
+				function()
+					require("telescope.builtin").keymaps()
+				end,
+				opts,
+			},
 			{ "<space>tm", "<cmd>Telescope media_files<cr>", opts },
 			{ "<space>nc", search_dotfiles, opts },
 			{ "<space>cw", change_wallpaper, opts },
 		},
 		config = function()
-      require("telescope").setup(config)
+			require("telescope").setup(config)
 			require("telescope").load_extension("fzf")
 			require("telescope").load_extension("media_files")
-      require("telescope").load_extension("notify")
+			require("telescope").load_extension("notify")
 		end,
 	},
 }
